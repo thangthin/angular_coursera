8ef4ac69281df59113d4cc1f8ca53409f52626cd
thangthin <thangthin@gmail.com>
2016-09-26 00:50:40 -0500

    bootstrapped app

---
+++ module2-solution/app.js
@@ -0,0 +1,17 @@
+"use strict";
+(function() {
+    var app = angular.module("ShoppingListCheckOff", []);
+
+    app.controller("ToBuyController", ["$scope", ToBuyController]);
+
+    app.controller("AlreadyBoughtController", ["$scope", AlreadyBoughtContoller]);
+
+    function ToBuyController() {
+
+    }
+
+    function AlreadyBoughtController() {
+
+    }
+
+}());
